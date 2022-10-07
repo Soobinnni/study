2cbd98bbbe6d360ffeac157a954838f113092e41
KIM SOOBIN <xpsxm225@naver.com>
2022-10-08 00:46:59 +0900

    221008FINAL

--- BasicExam/src/practiceProblem/MultiplicationTable.java
+++ BasicExam/src/practiceProblem/MultiplicationTable.java
@@ -4,13 +4,20 @@ public class MultiplicationTable {
 
 	public static void main(String[] args) {
 		// 구구단 프로그램
-		for (int j = 2; j < 10; j++) {
-			System.out.print(" ==" + j + "단" + "=="+"\t\t");
-			for (int i = 2; i < 10; i++) {
-				System.out.print(j + " X " + "1" + " = " + (j * 1)+"\t\t\n");
-				
+
+		for (int i = 2; i < 10; i++) {
+			System.out.print(i + "단\t");
+		}
+		System.out.println();
+
+		for (int k = 1; k < 10; k++) {
+			for (int j = 2; j < 10; j++) {
+				System.out.print(j + "*" + k + "=" + (j * k) + "\t");
+				switch (j) {
+				case 9:
+					System.out.println();
+				}
 			}
 		}
-
 	}
 }
